871ae0993cec8e3aa275adb205aeaeb92cf3f306
Loas717 <luisalbergoni717@gmail.com>
2025-05-30 22:50:46 -0300

    automatic board complete

--- main.c
+++ main.c
@@ -6,14 +6,17 @@
 
 #define MAX 10
 
+//para fazer: o jogo fica melhor se for manual msm, o srand produz numeros similares
 
 int printBoard(), jogar_dado();
-void jogador1(), jogador2();
+void jogador1(int readfd, int writefd), jogador2(int readfd, int writefd);
 
 int pos_j1=0, pos_j2=0, tabuleiro[MAX];
 
 int main(){
+    srand(time(NULL) + getpid());
     int pipe_j1[2], pipe_j2[2], pipe_j1_s[2], pipe_j2_s[2], descritorj1, descritorj2;
+    int quem_comeca = rand() % 2; // 0 = jogador1, 1 = jogador2
     if (pipe(pipe_j1) < 0 || pipe(pipe_j1_s) < 0 || pipe(pipe_j2) < 0 || pipe(pipe_j2_s) < 0) {
         printf("Erro na chamada PIPE");
         exit(1);
@@ -30,32 +33,118 @@ int main(){
             exit(1);
         }
         if(descritorj2==0){
-            jogador2();
+            close(pipe_j2[0]);
+            close(pipe_j1_s[1]);
+            close(pipe_j1[0]); 
+            close(pipe_j1[1]);
+            close(pipe_j1_s[0]); 
+            close(pipe_j1_s[1]);
+            jogador2(pipe_j2_s[0], pipe_j2[1]);
         }
     }else{
-        jogador1();
+        close(pipe_j1[0]);
+        close(pipe_j1_s[1]);
+        close(pipe_j2[0]); 
+        close(pipe_j2[1]);
+        close(pipe_j2_s[0]); 
+        close(pipe_j2_s[1]);
+        jogador1(pipe_j1_s[0], pipe_j1[1]);
+    }
+    close(pipe_j1[1]);    
+    close(pipe_j1_s[0]);   
+    close(pipe_j2[1]);    
+    close(pipe_j2_s[0]);
+
+    int dado, j_1=0,j_2=1;
+
+    printf("Quem começa? %d\n", quem_comeca);
+
+    while (1)
+    {
+        if(quem_comeca==0){
+            write(pipe_j1_s[1],&j_1,sizeof(int));
+            read(pipe_j1[0], &dado,sizeof(int));
+            pos_j1+=dado;
+            printf("Jogador 1 tirou %d e foi para posição %d\n", dado, pos_j1);
+            if(pos_j1>=MAX){
+                printf("Jogador 1 venceu!\n");
+                break;               
+            }
+
+            write(pipe_j2_s[1],&j_2,sizeof(int));
+            read(pipe_j2[0], &dado,sizeof(int));
+            pos_j2+=dado;
+            printf("Jogador 2 tirou %d e foi para posição %d\n", dado, pos_j2);
+            if(pos_j2>=MAX){
+                printf("Jogador 2 venceu!\n");
+                break;               
+            }
+        }else{
+            write(pipe_j2_s[1],&j_2,sizeof(int));
+            read(pipe_j2[0], &dado,sizeof(int));
+            pos_j2+=dado;
+            printf("Jogador 2 tirou %d e foi para posição %d\n", dado, pos_j2);
+            if(pos_j2>=MAX){
+                printf("Jogador 2 venceu!\n");
+                break;               
+            }
+            
+            write(pipe_j1_s[1],&j_1,sizeof(int));
+            read(pipe_j1[0], &dado,sizeof(int));
+            pos_j1+=dado;
+            printf("Jogador 1 tirou %d e foi para posição %d\n", dado, pos_j1);
+            if(pos_j1>=MAX){
+                printf("Jogador 1 venceu!\n");
+                break;               
+            }
+        }
     }
     
+
+    close(pipe_j1[0]);
+    close(pipe_j1_s[1]);
+    close(pipe_j2[0]);
+    close(pipe_j2_s[1]);
     //printf("main.c");
 }
 
 int jogar_dado() {
-    srand(time(NULL) + getpid());  
+    //srand(time(NULL) + getpid());  
     int resultado = (rand() % 6) + 1;
-    printf("Dado jogado! Valor obtido: %d\n", resultado);
+    //printf("Dado jogado! Valor obtido: %d\n", resultado);
     return resultado;
 }
 
-void jogador1(){
-    int dado = jogar_dado();
-    pos_j1 += dado;
-    printf("Jogador 1 está na posição %d\n", pos_j1);
+void jogador1(int readfd, int writefd){
+    srand(time(NULL) + getpid()); 
+    while (1)
+    {
+        int comando;
+        read(readfd, &comando, sizeof(int));
+        if(comando==0){
+            int dado = jogar_dado();
+            //pos_j1 += dado;
+            write(writefd, &dado, sizeof(int));
+            //printf("Jogador 1 está na posição %d\n", pos_j1);
+        }
+        
+    }
 }
 
-void jogador2(){
-    int dado = jogar_dado();
-    pos_j2 += dado;
-    printf("Jogador 2 está na posição %d\n", pos_j2);
+void jogador2(int readfd, int writefd){
+    srand(time(NULL) + getpid()); 
+    while (1)
+    {
+        int comando;
+        read(readfd, &comando, sizeof(int));
+        if(comando==1){
+            int dado = jogar_dado();
+            //pos_j2 += dado;
+            write(writefd, &dado, sizeof(int));
+            //printf("Jogador 2 está na posição %d\n", pos_j2);
+        }
+        
+    }
 }
 
 int printBoard(){

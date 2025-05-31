7b01a0a0e8f31bcd611647ddf1244f1a1d668873
Loas717 <luisalbergoni717@gmail.com>
2025-05-31 12:18:04 -0300

    manual update

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 //para fazer: o jogo fica melhor se for manual msm, o srand produz numeros similares
 
 int printBoard(), jogar_dado();
-void jogador1(int readfd, int writefd), jogador2(int readfd, int writefd);
+void jogador1(int readfd, int writefd), jogador2(int readfd, int writefd), capturaTeclado(int id);
 
 int pos_j1=0, pos_j2=0, tabuleiro[MAX];
 
@@ -62,15 +62,17 @@ int main(){
     while (1)
     {
         if(quem_comeca==0){
+            capturaTeclado(j_1);
             write(pipe_j1_s[1],&j_1,sizeof(int));
             read(pipe_j1[0], &dado,sizeof(int));
             pos_j1+=dado;
             printf("Jogador 1 tirou %d e foi para posição %d\n", dado, pos_j1);
             if(pos_j1>=MAX){
                 printf("Jogador 1 venceu!\n");
-                break;               
+                break;            
             }
 
+            capturaTeclado(j_2);
             write(pipe_j2_s[1],&j_2,sizeof(int));
             read(pipe_j2[0], &dado,sizeof(int));
             pos_j2+=dado;
@@ -80,6 +82,7 @@ int main(){
                 break;               
             }
         }else{
+            capturaTeclado(j_2);
             write(pipe_j2_s[1],&j_2,sizeof(int));
             read(pipe_j2[0], &dado,sizeof(int));
             pos_j2+=dado;
@@ -88,7 +91,8 @@ int main(){
                 printf("Jogador 2 venceu!\n");
                 break;               
             }
-            
+
+            capturaTeclado(j_1);
             write(pipe_j1_s[1],&j_1,sizeof(int));
             read(pipe_j1[0], &dado,sizeof(int));
             pos_j1+=dado;
@@ -127,7 +131,6 @@ void jogador1(int readfd, int writefd){
             write(writefd, &dado, sizeof(int));
             //printf("Jogador 1 está na posição %d\n", pos_j1);
         }
-        
     }
 }
 
@@ -144,6 +147,21 @@ void jogador2(int readfd, int writefd){
             //printf("Jogador 2 está na posição %d\n", pos_j2);
         }
         
+    }
+}
+
+void capturaTeclado(int id){
+    char input[10];
+    if(id==0){
+        do {
+            printf("Jogador 1, digite 'Y' para jogar o dado: ");
+            fgets(input, sizeof(input), stdin);
+        } while (input[0] != 'Y' && input[0] != 'y');
+    }else{
+        do {
+            printf("Jogador 2, digite 'Y' para jogar o dado: ");
+            fgets(input, sizeof(input), stdin);
+        } while (input[0] != 'Y' && input[0] != 'y');
     }
 }
 
